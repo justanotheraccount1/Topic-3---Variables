6264fab8f2670ad998c2db66ea8323ef446d345d
Sam Ahrens <339729139@GoTVDSB.ca>
2024-09-10 08:47:23 -0400

    Add project files.

---
+++ Topic 3 - Variables/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_3___Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName, myEyes, myTeeth, myHair
+        }
+    }
+}
